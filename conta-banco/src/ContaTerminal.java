6abceb139606bfce3942e7d2747ec8b4b1f3da4b
Natanael Silva <natanaelsilva.cc16@gmail.com>
2025-06-13 11:05:24 -0300

    Add conta-terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var scanner = new java.util.Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da agência:");
+        var agency = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da conta:");
+        var accountNumber = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        var clientName = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta:");
+        var balance = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("Olá " + clientName + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + accountNumber + " e seu saldo " + balance + " já está disponível para saque.");
     }
 }
